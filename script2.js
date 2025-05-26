14bcc78825955b6839740af28714facc1e98c4ec
PabloCampusano <campusanopablo990@gmail.com>
2025-05-26 14:02:56 -0400

    1
    1

---
+++ script2.js
@@ -0,0 +1,11 @@
+//validar nombre, debe contener solo letras y no ser vacio
+//validar edad: Debe ser mayor o igual a 18 y menor a 100
+
+function validar(){
+    const nombre = document.getElementById('nombre').value;
+    const edad = document.getElementById('edad').value;
+    
+    const cuerpoTabla = document.getElementById('cuerpoTabla').value;
+    
+
+}
