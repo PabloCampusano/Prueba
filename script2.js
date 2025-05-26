333e6f907d255569b148cb57a67104548ca0305d
PabloCampusano <campusanopablo990@gmail.com>
2025-05-26 14:32:46 -0400

    este
    este

--- script2.js
+++ script2.js
@@ -11,6 +11,10 @@ function validar(){
     nuevaFila.insertCell(0).innerText = nombre;
     nuevaFila.insertCell(1).innerText = edad;
     const celdaAccion = nuevaFila.insertCell(2);
+        const btnEliminar = document.createElement('button');
+        btnEliminar.innerText = 'Eliminar';
+        btnEliminar.onclick = function() {
+                tabla.deleteRow(nuevaFila.rowIndex - 1)
 
-
+}
 }
